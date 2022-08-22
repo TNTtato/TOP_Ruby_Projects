990009580c6583fb6b9a9d4a59c67af77b0b5e3f
gustavo <penarandagustavo5@gmail.com>
2022-08-22 11:28:10 -0500

    add generator of all posibilities

--- oop_projects/mastermind/game.rb
+++ oop_projects/mastermind/game.rb
@@ -117,4 +117,14 @@ class Game
     end
     n
   end
+
+  def gen_pos
+    base = (1111..6666).to_a
+    base_s = base.map { |num| num.to_s }
+    base_s_n = base_s.select do |num|
+      num.split('').any? { |n| n.to_i > 6 || n.to_i.zero? }
+    end
+    base_s -= base_s_n
+  end
+  
 end

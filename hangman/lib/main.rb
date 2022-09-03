f99c4a75338487d5cf00042d781eeb4ee41e30ad
gustavo <penarandagustavo5@gmail.com>
2022-09-03 16:59:08 -0500

    main aplication

--- hangman/lib/main.rb
+++ hangman/lib/main.rb
@@ -1,11 +1,11 @@
-# General functions
+require_relative 'game'
+require_relative 'interface'
 
-# 1. Load dictionary evary time a game is started
-# 2. A word of 5 to 12 caracters shoul be selected
-# 3. Each time of an input, if the caracter is correct, print the place and
-#    evary time is repeated in the word, if incorrect the chances are 
-#    decreased by one, and the previous guesses are shown as feedback
-# 4. Once the word is fully guessed, a congrats message should appear, otherwise
-#    if the chances are over, the original word must be printed
-# 5. A save game functionality must be added, this should keep the actual state
-#    of the game, and could be loaded at the start of each game, if the player wants to
+Interface.instructions
+loop do
+  Game.new
+  puts 'Do you want to play again? --> Enter: Y or y'
+  break unless gets.chomp.downcase == 'y'
+end
+
+puts 'Thanks for playing!'

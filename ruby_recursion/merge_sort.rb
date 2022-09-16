e7cb910b5d609e91a15393649e1717598ba9e94b
gustavo <penarandagustavo5@gmail.com>
2022-09-16 18:07:53 -0500

    merge sort

--- ruby_recursion/merge_sort.rb
+++ ruby_recursion/merge_sort.rb
@@ -1,2 +1,31 @@
-def merge_sort
+def merge_sort(elements)
+  return elements if elements.length < 2
+
+  first_half = elements[0..(elements.length - 1)/2]
+  second_half = elements[(elements.length - 1)/2 + 1..]
+  a = merge_sort(first_half)
+  b = merge_sort(second_half)
+  i = 0
+  j = 0
+  elements.each_with_index do |_elm, k|
+    if b[j].nil?
+      elements[k] = a[i]
+      i += 1
+    elsif a[i].nil?
+      elements[k] = b[j]
+      j += 1
+    elsif a[i] < b[j]
+      elements[k] = a[i]
+      i += 1
+    else
+      elements[k] = b[j]
+      j += 1
+    end
+  end
+  elements
 end
+
+n = 2220
+a = (-n..n).to_a.sample(10)
+p merge_sort(a)
+

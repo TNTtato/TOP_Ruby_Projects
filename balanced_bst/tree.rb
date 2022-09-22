b8c94140db8400ef767a5a404208412ce4e73a19
gustavo <penarandagustavo5@gmail.com>
2022-09-22 16:07:57 -0500

    insert #pretty_print

--- balanced_bst/tree.rb
+++ balanced_bst/tree.rb
@@ -17,6 +17,12 @@ class Tree
     return root
   end
 
-  
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
 
+root = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+root.pretty_print

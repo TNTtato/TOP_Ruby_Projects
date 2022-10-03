14e3f6aa97983f7e6909dc9f827dbd8f590cdd0f
gustavo <penarandagustavo5@gmail.com>
2022-10-03 16:13:06 -0500

    mv #create_board into class Board

--- knights_travails/board.rb
+++ knights_travails/board.rb
@@ -1,7 +1,9 @@
-def create_board
-  board = Array.new(64) {Array.new(2)}
-  .each_with_index do|pos, idx|
-      pos[0] = idx / 8
-      pos[1] = idx % 8
-    end
+class Board
+  def create_board
+    board = Array.new(64) {Array.new(2)}
+    .each_with_index do|pos, idx|
+        pos[0] = idx / 8
+        pos[1] = idx % 8
+      end
+  end
 end

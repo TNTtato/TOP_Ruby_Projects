4e8e418d8c00ccf2d278cf432073717058682d01
gustavo <penarandagustavo5@gmail.com>
2022-10-06 18:14:56 -0500

    build general graph

--- knights_travails/board.rb
+++ knights_travails/board.rb
@@ -1,9 +1,26 @@
 class Board
+  attr_accessor :knight, :graph
+
+  def initialize
+    @positions = create_board
+  end
+
   def create_board
-    board = Array.new(64) {Array.new(2)}
-    .each_with_index do|pos, idx|
-        pos[0] = idx / 8
-        pos[1] = idx % 8
+    Array.new(64) {Array.new(2)}
+         .each_with_index do |pos, idx|
+           pos[0] = idx / 8
+           pos[1] = idx % 8
+         end
+  end
+
+  def build_graph
+    @graph = @positions.map do |pos|
+      temp = []
+      self.knight.moves.each do |mov|
+        pos_change = [pos[0] + mov[0], pos[1] + mov[1]]
+        temp << @positions.index(pos_change) if @positions.include? pos_change
       end
+      temp
+    end
   end
 end
--- knights_travails/knight.rb
+++ knights_travails/knight.rb
@@ -1,2 +1,18 @@
+require_relative 'board'
+
 class Knight
+  attr_reader :board, :moves
+
+  def initialize(start, end_pos)
+    @start = start
+    @end = end_pos
+    @moves = [[2, -1], [2, 1], [1, 2], [-1, 2], [-2, -1], [-2, 1], [1, -2], [-1, -2]]
+    @board = Board.new
+    @board.knight = self
+  end
+
 end
+
+k = Knight.new([0,0], [3,5])
+k.board.build_graph
+k.board.graph.each_with_index {|row, idx| puts "#{idx} --> #{row}"}
